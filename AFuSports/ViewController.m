52610e62aa313cc4740bafbb2ac64b38c3c37ed0
kentchen1991 <csh@chenshaohaideMacBook-Air.local>
2015-12-24 16:52:34 +0800

    提交

--- AFuSports/ViewController.m
+++ AFuSports/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import <AFNetworking.h>
+#import <SVProgressHUD.h>
 @interface ViewController ()
 
 @end
@@ -16,9 +17,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor =[UIColor redColor];
     // Do any additional setup after loading the view, typically from a nib.
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [SVProgressHUD showErrorWithStatus:@"asdasd"];
+    });
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
